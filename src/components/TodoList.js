1fa5a1c188d20e18f7102e16a752fe30ee1407b8
samantha <lattechoi2@gmail.com>
2021-04-22 22:26:28 +0900

    Todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,25 +1,37 @@
 import React, {useState} from 'react'
 import TodoForm from './TodoForm';
+import Todo from './Todo';
 
 function TodoList() {
 
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([]);
 
     const addTodo = (todo) => {
         if(!todo.text || /^\s*$/.test(todo.text)) {
-            return 
+            return ;
         }
 
-        const newTodos = [todo, ...todos]
+        const newTodos = [todo, ...todos];
 
-        setTodos(newTodos)
-        console.log(...todos);
+        setTodos(newTodos);
+    };
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
     }
 
     return (
         <div>
             <h1>What's the Plan for today !</h1>
             <TodoForm onSubmit={addTodo} />
+            <Todo todos={todos} completeTodo={completeTodo} />
+
         </div>
     )
 }
