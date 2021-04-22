c63a8e777c6e166ce4d14a4b4f91623fef69f302
samantha <lattechoi2@gmail.com>
2021-04-22 23:03:15 +0900

    todo 업데이트 기능

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -16,12 +16,19 @@ function TodoList() {
         setTodos(newTodos);
     };
 
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)) {
+            return;
+        }
+
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+    }
+
     const removeTodo = id => {
         const removeArr = [...todos].filter(todo => todo.id !== id)
 
         setTodos(removeArr);
     }
-
 
     const completeTodo = id => {
         let updatedTodos = todos.map(todo => {
@@ -37,7 +44,7 @@ function TodoList() {
         <div>
             <h1>What's the Plan for today !</h1>
             <TodoForm onSubmit={addTodo} />
-            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
 
         </div>
     )
